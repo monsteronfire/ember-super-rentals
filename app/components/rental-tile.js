ae84844e288f9277ed1be49ba7ed4bd67d87d8e1
monsteronfire <monsteronfire@gmail.com>
2016-09-09 16:05:07 +0800

    Add actions to toggle visibility of rental.image

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isImageShowing: false,
+
+  actions: {
+    imageShow: function () {
+      Ember.set(this, 'isImageShowing', true);
+    },
+
+    imageHide: function () {
+      Ember.set(this, 'isImageShowing', false);
+    }
+  }
 });
