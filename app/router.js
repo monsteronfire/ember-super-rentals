bfa416ba3d4298136574ec8e7bb58db3a210806a
monsteronfire <monsteronfire@gmail.com>
2016-09-09 12:56:10 +0800

    Update ember-cli
    * Also restart the project by removing old files

--- app/router.js
+++ app/router.js
@@ -2,12 +2,11 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType
+  location: config.locationType,
+  rootURL: config.rootURL
 });
 
 Router.map(function() {
-  this.route('about');
-  this.route('contact');
 });
 
 export default Router;
